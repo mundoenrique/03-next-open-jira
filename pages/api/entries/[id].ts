45d509d494465d4b00a4633a1317f9295150b4c0
mundoenrique <mundoenrique@hotmail.com>
2022-07-22 19:51:13 -0400

    Obtiene entrada por ID

--- pages/api/entries/[id].ts
+++ pages/api/entries/[id].ts
@@ -18,11 +18,29 @@ export default function handler(
   switch (req.method) {
     case 'PUT':
       return updateEntry(req, res);
+    case 'GET':
+      return getEntry(req, res);
 
     default:
       return res.status(400).json({ message: 'Método no existe' });
   }
 }
+
+const getEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { id } = req.query;
+
+  await db.connect();
+  const entryInDB = await Entry.findById(id);
+  await db.disconnect();
+
+  if (!entryInDB) {
+    return res
+      .status(400)
+      .json({ message: 'No hay entrada con ese ID: ' + id });
+  }
+
+  return res.status(200).json(entryInDB);
+};
 
 const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   const { id } = req.query;
